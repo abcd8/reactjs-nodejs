bc7655c20e6b30adcb4066270a47471a81926339
abcd8 <brianbui.n9@gmail.com>
2020-02-28 08:09:24 +0700

    register user and add jsonwebtoken

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
 
 /*
     @route GET api/users
     @des Test route
     @access Public
 */
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
     res.send('Auth route')
 })
 
